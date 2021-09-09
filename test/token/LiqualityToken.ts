8a43715fb305a809aedc1060097f04d8d3a4507d
Krasimir Raykov <raykov@weichain.tech>
2021-09-09 12:56:21 +0300

    refactor: improve the way token is deployed during tests

--- test/token/LiqualityToken.ts
+++ test/token/LiqualityToken.ts
@@ -1,25 +1,21 @@
-import hre from 'hardhat'
-import { Artifact } from 'hardhat/types'
+import { ethers } from 'hardhat'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
 
 import { LiqualityToken } from '../../typechain/LiqualityToken'
 import { shouldBehaveLikeErc20 } from './LiqualityToken.behavior'
 
-const { deployContract } = hre.waffle
-
 describe('Liquality Token', function () {
   before(async function () {
-    const signers: SignerWithAddress[] = await hre.ethers.getSigners()
+    const signers: SignerWithAddress[] = await ethers.getSigners()
     this.signers = {} as SignerWithAddress
     this.signers.admin = signers[0]
     this.signers.sender = signers[1]
     this.signers.receiver = signers[2]
 
     const initialMinter: string = this.signers.admin.address
-    const greeterArtifact: Artifact = await hre.artifacts.readArtifact('LiqualityToken')
-    this.liqToken = <LiqualityToken>(
-      await deployContract(this.signers.admin, greeterArtifact, [initialMinter])
-    )
+
+    const LiqualityTokenFactory = await ethers.getContractFactory('LiqualityToken')
+    this.liqToken = <LiqualityToken>await LiqualityTokenFactory.deploy(initialMinter)
   })
 
   shouldBehaveLikeErc20()
