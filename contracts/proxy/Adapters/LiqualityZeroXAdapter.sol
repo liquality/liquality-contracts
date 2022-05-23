f7038d6022527ce74252ad39a65fb3c9deb28565
koderholic <22542946+koderholic@users.noreply.github.com>
2022-05-23 12:54:57 +0100

    refactor: 1)optimize if structure, 2) wrap event data in struct

--- contracts/proxy/Adapters/LiqualityZeroXAdapter.sol
+++ contracts/proxy/Adapters/LiqualityZeroXAdapter.sol
@@ -8,15 +8,23 @@ import "../interfaces/ILiqualityProxyAdapter.sol";
 contract LiqualityZeroXAdapter is ILiqualityProxyAdapter {
     /// @notice This works for ZeroX sellToUniswap.
     function swap(
-        uint256 feeRate,
+        uint256 _feeRate,
         address feeCollector,
         LiqualityProxySwapParams calldata swapParams
     ) external payable {
         // Determine the swap type(fromToken or fromValue) and initiate swap.
         bytes memory response;
+        uint256 returnedAmount;
         if (Adapter.isSwapFromValue()) {
             // If it's a swap from value
             response = Adapter.beginFromValueSwap(swapParams.target, swapParams.data);
+            returnedAmount = abi.decode(response, (uint256));
+            Adapter.handleReturnedToken(
+                swapParams.tokenOut,
+                returnedAmount,
+                _feeRate,
+                feeCollector
+            );
         } else {
             // If it's a swap from Token
             response = Adapter.beginFromTokenSwap(
@@ -25,25 +33,32 @@ contract LiqualityZeroXAdapter is ILiqualityProxyAdapter {
                 swapParams.amountIn,
                 swapParams.data
             );
-        }
-        uint256 returnedAmount = abi.decode(response, (uint256));
+            returnedAmount = abi.decode(response, (uint256));
 
-        // handle returnedAmount
-        if (Adapter.isSwapToValue(swapParams.tokenOut)) {
-            Adapter.handleReturnedValue(returnedAmount, feeRate, payable(feeCollector));
-        } else {
-            // If it's a swap to token
-            Adapter.handleReturnedToken(swapParams.tokenOut, returnedAmount, feeRate, feeCollector);
+            // handle returnedAmount
+            if (Adapter.isSwapToValue(swapParams.tokenOut)) {
+                Adapter.handleReturnedValue(returnedAmount, _feeRate, payable(feeCollector));
+            } else {
+                // If it's a swap to token
+                Adapter.handleReturnedToken(
+                    swapParams.tokenOut,
+                    returnedAmount,
+                    _feeRate,
+                    feeCollector
+                );
+            }
         }
 
-        emit LiqualityProxySwap(
-            swapParams.target,
-            msg.sender,
-            feeRate,
-            swapParams.tokenIn,
-            swapParams.tokenOut,
-            swapParams.amountIn,
-            returnedAmount
-        );
+        LiqualityProxySwapInfo memory swapInfo = LiqualityProxySwapInfo({
+            target: swapParams.target,
+            user: msg.sender,
+            feeRate: _feeRate,
+            tokenIn: swapParams.tokenIn,
+            tokenOut: swapParams.tokenOut,
+            amountIn: swapParams.amountIn,
+            amountOut: returnedAmount
+        });
+
+        emit LiqualityProxySwap(swapInfo);
     }
 }
--- contracts/proxy/interfaces/ILiqualityProxyAdapter.sol
+++ contracts/proxy/interfaces/ILiqualityProxyAdapter.sol
@@ -4,16 +4,18 @@ pragma solidity >=0.8.10;
 import "./IProxyCommons.sol";
 
 interface ILiqualityProxyAdapter is IProxyCommons {
+    struct LiqualityProxySwapInfo {
+        address target;
+        address user;
+        uint256 feeRate;
+        address tokenIn;
+        address tokenOut;
+        uint256 amountIn;
+        uint256 amountOut;
+    }
+
     /// @dev Emitted when a successful swap operation goes throuth the proxy.
-    event LiqualityProxySwap(
-        address indexed target,
-        address indexed user,
-        uint256 indexed feeRate,
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn,
-        uint256 amountOut
-    );
+    event LiqualityProxySwap(LiqualityProxySwapInfo swapInfo);
 
     function swap(
         uint256 feeRate,
