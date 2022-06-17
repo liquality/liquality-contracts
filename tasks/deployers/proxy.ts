23695783980430adcb195441a65d710d3fd502e0
koderholic <22542946+koderholic@users.noreply.github.com>
2022-06-17 23:31:10 +0100

    fix: update proxy deploy script

--- tasks/deployers/proxy.ts
+++ tasks/deployers/proxy.ts
@@ -8,6 +8,8 @@ dotenvConfig({ path: resolve(__dirname, '../../.env') })
 import {
   Liquality1InchAdapter,
   Liquality1InchAdapter__factory,
+  LiqualityHTLCAdapter,
+  LiqualityHTLCAdapter__factory,
   LiqualityProxy,
   LiqualityProxy__factory,
   LiqualityZeroXAdapter,
@@ -16,8 +18,11 @@ import {
 
 export const SWAPPERS = {
   ONE_INCH_AGGREGATORV4: '0x1111111254fb6c44bAC0beD2854e76F90643097d'.toLowerCase(), //Verified
-  ZEROX: '0xDef1C0ded9bec7F1a1670819833240f027b25EfF'.toLowerCase() //  Verified
+  ZEROX: '0xDef1C0ded9bec7F1a1670819833240f027b25EfF'.toLowerCase(), //  Verified
+  HTLC: '0x133713376F69C1A67d7f3594583349DFB53d8166'.toLowerCase()
 }
+
+const wEth = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 
 task('deployLiqualityProxy').setAction(async function (taskArguments: TaskArguments, { ethers }) {
   console.log('================= Deploying Liquality Proxy =================')
@@ -26,9 +31,7 @@ task('deployLiqualityProxy').setAction(async function (taskArguments: TaskArgume
   const admin = await signer1.getAddress()
 
   const liqProxyFactory: LiqualityProxy__factory = await ethers.getContractFactory('LiqualityProxy')
-  const liqProxy: LiqualityProxy = <LiqualityProxy>(
-    await liqProxyFactory.deploy(admin, 1000, feeCollector)
-  )
+  const liqProxy: LiqualityProxy = <LiqualityProxy>await liqProxyFactory.deploy(admin, feeCollector)
   await liqProxy.deployed()
   console.log('Liquality Proxy deployed to: ', liqProxy.address)
 
@@ -44,12 +47,15 @@ task('deployLiqualityProxy').setAction(async function (taskArguments: TaskArgume
   await (await liqProxy.addAdapter(SWAPPERS.ZEROX, liqZeroXAdapter.address)).wait()
   console.log('liqZeroXAdapter added to proxy')
 
+  await (await liqProxy.setFeeRate(1000, SWAPPERS.ZEROX)).wait()
+  console.log('liqZeroXAdapter feeRate added to proxy')
+
   console.log('================= Deploying Liquality1InchAdapter =================')
   const Liquality1InchAdapterFactory: Liquality1InchAdapter__factory = <
     Liquality1InchAdapter__factory
   >await ethers.getContractFactory('Liquality1InchAdapter')
   const liquality1InchAdapter: Liquality1InchAdapter = <Liquality1InchAdapter>(
-    await Liquality1InchAdapterFactory.deploy()
+    await Liquality1InchAdapterFactory.deploy(wEth)
   )
   await liquality1InchAdapter.deployed()
   console.log('Liquality1InchAdapter deployed to: ', liquality1InchAdapter.address)
@@ -58,4 +64,23 @@ task('deployLiqualityProxy').setAction(async function (taskArguments: TaskArgume
     await liqProxy.addAdapter(SWAPPERS.ONE_INCH_AGGREGATORV4, liquality1InchAdapter.address)
   ).wait()
   console.log('Liquality1InchAdapter added to proxy')
+
+  await (await liqProxy.setFeeRate(1000, SWAPPERS.ONE_INCH_AGGREGATORV4)).wait()
+  console.log('Liquality1InchAdapter feeRate added to proxy')
+
+  console.log('================= Deploying LiqualityHTLCAdapter =================')
+  const LiqualityHTLCAdapterFactory: LiqualityHTLCAdapter__factory = <
+    LiqualityHTLCAdapter__factory
+  >await ethers.getContractFactory('LiqualityHTLCAdapter')
+  const liqualityHTLCAdapter: LiqualityHTLCAdapter = <LiqualityHTLCAdapter>(
+    await LiqualityHTLCAdapterFactory.deploy()
+  )
+  await liqualityHTLCAdapter.deployed()
+  console.log('LiqualityHTLCAdapter deployed to: ', liqualityHTLCAdapter.address)
+
+  await (await liqProxy.addAdapter(SWAPPERS.HTLC, liqualityHTLCAdapter.address)).wait()
+  console.log('LiqualityHTLCAdapter added to proxy')
+
+  await (await liqProxy.setFeeRate(1000, SWAPPERS.HTLC)).wait()
+  console.log('LiqualityHTLCAdapter feeRate added to proxy')
 })
