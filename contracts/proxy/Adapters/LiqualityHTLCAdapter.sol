793657b1b2f135b63cedff1f8c38e0ea1bf275ca
koderholic <22542946+koderholic@users.noreply.github.com>
2022-06-17 23:22:14 +0100

    feat: add adapter for liquality htlc

---
+++ contracts/proxy/Adapters/LiqualityHTLCAdapter.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.10;
+
+import "../Libraries/AtomicSwap.sol";
+import "../interfaces/ISwapperAdapter.sol";
+
+contract LiqualityHTLCAdapter is ISwapperAdapter, AtomicSwap {
+    struct HTLCData {
+        uint256 amount;
+        uint256 expiration;
+        bytes32 secretHash;
+        address tokenAddress;
+        address refundAddress;
+        address recipientAddress;
+    }
+
+    /// @notice This works for ZeroX sellToUniswap.
+    function swap(
+        uint256 feeRate,
+        address feeCollector,
+        address target,
+        bytes calldata data
+    ) external payable {
+        // Decode data
+        HTLCData memory htlc;
+        (htlc) = abi.decode(data[4:], (HTLCData));
+
+        // We arbitrarily use address(1) as tokenOut is unknown
+        execute(feeRate, feeCollector, target, data, htlc.tokenAddress, address(1), htlc.amount);
+    }
+}
